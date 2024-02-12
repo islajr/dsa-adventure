3e5f383ab135cbfdc3e80da06e96ab2b0e56c313
Isla <islarcherjr@gmail.com>
2024-02-12 18:24:49 +0100

    cpp and introduction to data structures in java

---
+++ cpp/intro/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+enum CardSuits
+{
+    Club, 
+    Diamond, 
+    Heart, 
+    Spade
+};
+
+enum CardElements
+{
+    Ace, 
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen, 
+    King
+};
+
+int main()
+{
+    enum Gender {
+        Male,
+        Female
+    };
+
+    return 0;
+}
---
+++ java/data-structures/data-structures.md
@@ -0,0 +1,11 @@
+# Data Structures in Java
+
+Data structures are a way of organizing data in a computer memory to ensure they are **saved** and **accessed** efficiently.
+
+## Classification
+
+There are two main classifications of data structures, namely:
+
+* Linear Data structures
+* Non-linear data structures
+  
