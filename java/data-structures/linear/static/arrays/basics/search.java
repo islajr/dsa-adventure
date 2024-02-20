a9e372b2bc1586e5da966f0df6fc96cce36322bf
Isla <islarcherjr@gmail.com>
2024-02-20 22:10:46 +0100

    added loop feature.

--- java/data-structures/linear/static/arrays/basics/search.java
+++ java/data-structures/linear/static/arrays/basics/search.java
@@ -11,21 +11,22 @@ public class search{
         for (int i = 0; i < numbers.length; ++i) {
             numbers[i] = random.nextInt(101);
         }
-
+        while (true) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Input a number to search: ");
         int response = scanner.nextInt();
-        scanner.close();
 
         for (int i = 0; i < numbers.length; ++i) {
             if (response == numbers[i]) {
                 System.out.println("The index of the number is " + i + ".");
-                break;
+                scanner.close();
+                System.exit(0);
             }
             else {
                 System.out.println("This number is not in the array");
                 break;
             }
+        }
         }
     }
 }
