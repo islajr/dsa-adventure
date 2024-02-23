f45e71c05c6dcad860472f6562b16690c70107e7
Isla <islarcherjr@gmail.com>
2024-02-23 10:53:33 +0100

    working insertion.

--- java/data-structures/linear/static/arrays/basics/unsorted.java
+++ java/data-structures/linear/static/arrays/basics/unsorted.java
@@ -1,12 +1,14 @@
-import java.util.Arrays;
-
 public class unsorted {
     public static void main(String[] args) {
         // program to search, insert and delete elements from an unsorted array
 
         int[] unsorted = {2, 1, 4, 6, 3};
 
-        insert(unsorted);
+        int[] newArray = insert(unsorted, 5);
+
+        for (int i : newArray) {
+            System.out.println(i);
+        }
 
 
 
@@ -28,5 +30,21 @@ public class unsorted {
         else {
             System.out.println(value + " was not found.");
         }
+    }
+
+    //  function fot inserting
+    public static int[] insert(int[] array, int value) {
+        int[] newArray = new int[array.length + 1];
+
+        for (int i = 0; i < newArray.length; ++i) {
+            if (i == newArray.length - 1) {
+                newArray[i] = value;
+            }
+            else {
+                newArray[i] = array[i];
+            }
+        }
+        
+        return newArray;
     }
 }
