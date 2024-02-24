934ec05a62028fb01d36a9fc2e5a388e2ed7975a
Isla <islarcherjr@gmail.com>
2024-02-24 19:48:07 +0100

    prototype mini-program with working search method.

--- java/data-structures/linear/static/arrays/basics/unsorted.java
+++ java/data-structures/linear/static/arrays/basics/unsorted.java
@@ -1,17 +1,66 @@
+import java.util.Scanner;
+
 public class unsorted {
     public static void main(String[] args) {
         // program to search, insert and delete elements from an unsorted array
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Hello there, I'm to create an array for you.");
+            System.out.print("How long do you want your array to be? ");
+            int length = scanner.nextInt();
 
-        int[] unsorted = {2, 1, 4, 6, 3};
+            // creating the array with the desired length
+            int[] array = new int[length];
+            for (int i = 0; i < length; ++i) {
+                System.out.print("Please input a desired array value: ");
+                array[i] = scanner.nextInt();
+            }
 
-        int[] newArray = delete(unsorted, 6);
+            /* // printing out the array
+            System.out.print("Your array: {");
+            for (int i = 0; i < length; ++i) {
+                if (i == length - 1) {
+                    System.out.print(i + "}\n");
+                }
+                else {
+                    System.out.print(i + ", ");
+                }
+            } */
+            arrayToStdout(array);
 
-        for (int i : newArray) {
-            System.out.println(i);
+            // asking for the next step
+            System.out.println("What do you wish to do with your array?");
+            System.out.print("(S)Search, (I)Insert, (D)Delete, (Q)Quit. ");
+            String response = scanner.next();
+            response = response.strip().toLowerCase();
+
+            switch (response) {
+                case "q":
+                    System.out.println("See you later, mate.");
+                    System.exit(0);
+                case "s":
+                    System.out.print("What element are you looking to find? ");
+                    int searchAnswer = scanner.nextInt();
+                    search(array, searchAnswer);
+                    System.exit(0);
+                case "i":
+                    System.out.print("What number do you want to add to the array? ");
+                    int insertAnswer = scanner.nextInt();
+                    insert(array, insertAnswer);
+                    break;
+                case "d":
+                    System.out.print("What element don't you like? ");
+                    int deleteAnswer = scanner.nextInt();
+                    delete(array, deleteAnswer);
+                    break;
+                default:
+                    break;
+            }
+            scanner.close();
+
+
         }
-
-
-
+        
     }
 
     // searching function
@@ -32,7 +81,7 @@ public class unsorted {
         }
     }
 
-    //  function fot inserting
+    //  function for inserting
     public static int[] insert(int[] array, int value) {
         int[] newArray = new int[array.length + 1];
 
@@ -68,5 +117,18 @@ public class unsorted {
             System.out.println("This value is not in the array!");
         }
         return newArray;
+    }
+
+    public static void arrayToStdout(int[] array) {
+        // printing out the array
+        System.out.print("Your array: {");
+        for (int i = 0; i < array.length; ++i) {
+            if (i == array.length - 1) {
+                System.out.print(array[i] + "}\n");
+            }
+            else {
+                System.out.print(array[i] + ", ");
+            }
+        }
     }
 }
