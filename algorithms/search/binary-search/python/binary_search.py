bb9a1403d76cfdc4ba3eb043378179101522d878
Isla <islarcherjr@icloud.com>
2025-12-26 04:38:34 +0100

    python: binary search

---
+++ algorithms/search/binary-search/python/binary_search.py
@@ -0,0 +1,16 @@
+def binary_search(list, item):
+    low = 0
+    high = len(list) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        guess = list[mid]
+
+        if guess == item:
+            return mid
+        elif guess < item:
+            low = mid + 1
+        elif guess > item:
+            high = mid - 1
+    return None;
+            
