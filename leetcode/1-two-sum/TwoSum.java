9022ec3d3be1811c4ab35240642bcc41936eb490
Isla <islarcherjr@icloud.com>
2025-12-30 08:13:09 +0100

    leetcode: 1 - two sum

--- leetcode/1-two-sum/TwoSum.java
+++ leetcode/1-two-sum/TwoSum.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 class TwoSum {
     /* *
      * Given an array of integers 'nums' and an integer 'target', return the indices of the two numbers such that they add up to the 'target'.
@@ -5,22 +7,19 @@ class TwoSum {
      * You can return the answer in any order.
      */
     public int[] twoSum(int[] nums, int target) {
-        int[] answer = new int[2];
-        for (int i = 0; i < nums.length; ++i) {
-            for (int j = i + 1; j < nums.length; ++j) {
-                if (verify(target, nums[i], nums[j])) {
-                    answer[0] = i;
-                    answer[1] = j;
-                    System.out.println("[" + i + ", " + j + "]");
-                    return answer;
-                }
-            }
-        }
-        System.out.println("could not compute. check the values again!");
-        return null;
-    }
+        HashMap<Integer, Integer> records = new HashMap<>();
+        int difference;
 
-    private boolean verify(int target, int i, int j) {
-        return ((i + j) == target);
+        for (int i = 0; i < nums.length; ++i) {
+            difference = target - nums[i];
+
+            if (records.get(difference) == null)
+                records.put(nums[i], i);
+            else
+                return new int[] {records.get(difference), i};
+
+        }
+
+        return null;
     }
 }
