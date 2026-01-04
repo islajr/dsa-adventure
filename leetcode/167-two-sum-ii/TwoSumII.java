f4562d545b0d8904c6d4c6c3fdbf112b66e5caa3
Isla <islarcherjr@icloud.com>
2026-01-04 10:53:35 +0100

    leetcode: 167 - two sum ii

---
+++ leetcode/167-two-sum-ii/TwoSumII.java
@@ -0,0 +1,31 @@
+/* * 
+ * LC 167: Two Sum II - Input Array Is Sorted
+ * 
+ * Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. 
+ * Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+
+ * Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+*/
+public class TwoSumII {
+    public static int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+            if (numbers[left] + numbers[right] == target)
+                return new int[]{left + 1, right + 1};
+            else if (numbers[left] + numbers[right] > target)
+                right--;
+            else if (numbers[left] + numbers[right] < target)
+                left++;
+        }
+
+        return null;
+    }
+    
+    public static void main(String[] args) {
+        int[] result = twoSum(new int[]{-1, 0}, -1);
+        for (int i : result)
+            System.out.print(i);
+    }
+}
