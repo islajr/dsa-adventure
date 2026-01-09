878ee04016d49807fe2191e677baf6b977973823
Isla <islarcherjr@icloud.com>
2026-01-09 11:26:27 +0100

    leetcode: 344 - reverse string

---
+++ leetcode/344-reverse-string/ReverseString.java
@@ -0,0 +1,20 @@
+/* * 
+ * LC 344: Reverse String
+ * Write a function that reverses a string. The input string is given as an array of characters s.
+ * You must do this by modifying the input array in-place with O(1) extra memory.
+*/
+public class ReverseString {
+    public void revereString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+        
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right --;
+        }
+    }
+}
