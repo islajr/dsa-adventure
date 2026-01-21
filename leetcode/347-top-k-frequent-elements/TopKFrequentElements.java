d2bd24ca490a8965644fe22caaf54c6ec92eb477
Isla <islarcherjr@icloud.com>
2026-01-21 18:17:10 +0100

    leetcode: 347 - top k frequent elements

---
+++ leetcode/347-top-k-frequent-elements/TopKFrequentElements.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/* * 
+ * LC 347: Top K Frequent Elements
+ * 
+ * Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+*/
+public class TopKFrequentElements {
+    public int[] frequentElements(int[] nums, int k) {
+        Map<Integer, Integer> count = new HashMap<>();
+        List<Integer>[] freq = new List[nums.length + 1];
+        
+        for (int i = 0; i < freq.length; i++)
+            freq[i] = new ArrayList<>();
+
+        for (int n : nums)
+            count.put(n, count.getOrDefault(n, 0) + 1);
+
+        for (Map.Entry<Integer, Integer> entry : count.entrySet())
+            freq[entry.getValue()].add(entry.getKey());
+
+        int[] res = new int[k];
+        int index = 0;
+        
+        for (int i = freq.length - 1; i > 0 && index < k; i--) {
+            for (int n : freq[i]) {
+                res[index++] = n;
+                if (index == k)
+                    return res;
+            }
+        }
+
+        return res;
+    }
+}
