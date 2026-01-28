15f58b7f3615000d61b855aca57462a1af9e3a2e
Isla <islarcherjr@icloud.com>
2026-01-28 15:53:17 +0100

    leetcode: 881 - boats to save people

---
+++ leetcode/881-boats-to-save-people/BoatsToSavePeople.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+/* * 
+ * LC 881: Boats To Save People
+ * 
+ * You are given an array people where people[i] is the weight of the ith person, 
+ * and an infinite number of boats where each boat can carry a maximum weight of limit. 
+ * 
+ * Each boat carries at most two people at the same time, provided the sum of the weight of those people is at most limit.
+
+ * Return the minimum number of boats to carry every given person.
+*/
+public class BoatsToSavePeople {
+    public int minBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int result = 0, left = 0, right = people.length - 1;
+
+        while (left <= right) {
+            int diff = limit - people[right--];
+            result++;
+
+            if (left <= right && diff >= people[left])
+                left++;
+        }
+
+        return result;
+    }
+}
