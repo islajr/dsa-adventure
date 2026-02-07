daa0d7231fe533046d05618f57d3b69bf60793ab
Isla <islarcherjr@icloud.com>
2026-02-07 22:16:19 +0100

    leetcode: 567 - permutation in string (sliding window)

---
+++ leetcode/567-permutation-in-string/PermutationInStringII.java
@@ -0,0 +1,54 @@
+/* * 
+ * LC 567: Permutation in String
+ * 
+ * Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+
+ * In other words, return true if one of s1's permutations is the substring of s2.
+*/
+public class PermutationInStringII {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+
+        int[] s1Count = new int[26];
+        int[] s2Count = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            s1Count[s1.charAt(i) - 'a']++;
+            s2Count[s2.charAt(i) - 'a']++;
+        }
+
+        int matches = 0;
+        for (int i = 0; i < 26; i++) {
+            if (s1Count[i] == s2Count[i]) {
+                matches++;
+            }
+        }
+
+        // sliding window
+        int l = 0;
+        for (int r = s1.length(); r < s2.length(); r++) {
+            if (matches == 26) {
+                return true;
+            }
+
+            int index = s2.charAt(r) - 'a';
+            s2Count[index]++;
+            if (s1Count[index] == s2Count[index]) {
+                matches++;
+            } else if (s1Count[index] + 1 == s2Count[index]) {
+                matches--;
+            }
+
+            index = s2.charAt(l) - 'a';
+            s2Count[index]--;
+            if (s1Count[index] == s2Count[index]) {
+                matches++;
+            } else if (s1Count[index] - 1 == s2Count[index]) {
+                matches--;
+            }
+            l++;
+        }
+        return matches == 26;
+    }
+}
