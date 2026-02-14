8cfbe3d5a70a67c13b9513eca287b7dbfd0125a7
Isla <islarcherjr@icloud.com>
2026-02-14 17:25:08 +0100

    leetcode: 15 - 3sum

---
+++ leetcode/15-3sum/3sum.py
@@ -0,0 +1,37 @@
+""" 
+LC 15: 3Sum
+
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+"""
+class Solution:
+    def three_sum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()    
+        res = []
+        
+        for i, a in enumerate(nums):
+            if a > 0:
+                break
+            
+            if i > 0 and a == nums[l - 1]:
+                continue
+            
+            l, r = i + 1, len(nums) - 1
+            
+            while l < r:
+                three_sum = a + nums[l] + nums[r]
+                
+                if three_sum > 0:
+                    r -= 1
+                elif three_sum < 0:
+                    l += 1
+                else:
+                    res.append([a, nums[l], nums[r]])
+                    r -= 1
+                    l += 1
+                    while nums[l] == nums[l - 1] and  l < r:
+                        l += 1
+        return res 
+    
