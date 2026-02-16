dee6ca1dadb514cc5cb321946661307d36dbf242
Isla <islarcherjr@icloud.com>
2026-02-16 19:15:25 +0100

    leetcode: 143 - reorder linked list

---
+++ leetcode/143-reorder-linked-list/reorder_list.py
@@ -0,0 +1,37 @@
+"""
+LC 143: Reorder Linked List
+You are given the head of a singly linked-list. The list can be represented as:
+
+L0 → L1 → … → Ln - 1 → Ln
+Reorder the list to be on the following form:
+
+L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+
+"""
+class Solution:
+    def reorder_list(self, head: Optional[ListNode]) -> none:
+        # find the mid-point
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        # reverse second half
+        second = slow.next
+        prev = slow.next = None
+
+        while second:
+            temp = second.next
+            second.next = prev
+            prev = second
+            second = temp
+
+        # merge both halves alternately
+        first, second = head, prev
+        while second:
+            temp1, temp2 = first.next, second.next
+            first.next = second
+            second.next = temp1
+            first, second = temp1, temp2
+
