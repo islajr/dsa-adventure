392f1dc9c2e70c5620096b2a9c2a0aafb6992692
Isla <islarcherjr@icloud.com>
2026-02-21 22:44:49 +0100

    leetcode: 81 - search in rotated sorted array ii

---
+++ leetcode/81-search-in-rotated-sorted-array-ii/README.md
@@ -0,0 +1,13 @@
+# LC 81: Search in Rotated Sorted Array II
+
+> **Description** [LC 81](https://leetcode.com/problems/search-in-rotated-sorted-array-ii/description/)
+> **Difficulty:** Medium
+> **Time Complexity:** O(n)
+
+## Approach
+
+The rotated nature of the list `nums` alludes to the idea that at least one part of the list is sorted.
+
+This approach adapts binary search to this idea by comparing the median to both extremes indexes of the list and looking through each partition as needed.
+
+In cases where `nums[l] == nums[m]`, and it is impossible to determine the sorted partition the left pointer is incremented sequentially till it becomes possible.
---
+++ leetcode/81-search-in-rotated-sorted-array-ii/search_rotated_array.py
@@ -0,0 +1,27 @@
+""" 
+LC 81: Search in Rotated Sorted Array II
+"""
+class Solution:
+    def search(self, nums: list[int], target: int) -> bool:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            m = (l + r) // 2
+
+            if nums[m] == target:
+                return True
+            
+            if nums[l] < nums[m]: # left portion is sorted
+                if nums[l] <= target < nums[m]:
+                    r = m - 1
+                else:
+                    l = m + 1
+            elif nums[l] > nums[m]: # right portion is sorted
+                if nums[m] < target <= nums[r]:
+                    l = m + 1
+                else:
+                    r = m - 1
+            else:   # duplicates case
+                l += 1
+        
+        return False
