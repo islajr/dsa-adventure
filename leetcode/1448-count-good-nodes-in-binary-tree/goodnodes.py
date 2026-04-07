116607565859445a3bcd5c3cd3b0ece02a5fd2bd
Isla <islarcherjr@icloud.com>
2026-04-07 23:21:40 +0100

    leetcode: 1448 - count good nodes in binary tree

---
+++ leetcode/1448-count-good-nodes-in-binary-tree/goodnodes.py
@@ -0,0 +1,17 @@
+# LC 1448: Count Good Nodes in Binary Tree
+
+class Solution:
+    def goodnodes(self, root: TreeNode) -> int:
+        
+        def dfs(node, max_val):
+            if not node:
+                return 0
+
+            res = 1 if node.val >= max_val else 0
+            max_val = max(max_val, node.val)
+            res += dfs(node.left, max_val)
+            res += dfs(node.right, max_val)
+            return res
+
+        return dfs(root, root.val)
+
