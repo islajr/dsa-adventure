eab72115fa9c949326ac2dc95f51d978746d4fba
Isla <islarcherjr@icloud.com>
2026-04-14 23:25:22 +0100

    leetcode: 1325 - delete leaves with a given value (java)

---
+++ leetcode/1325-delete-leaves-with-a-given-value/DeleteLeaf.java
@@ -0,0 +1,36 @@
+class DeleteLeaf {
+
+	class TreeNode {
+		TreeNode left;
+		TreeNode right;
+		int val;
+
+		TreeNode() {}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	
+	}
+
+	public TreeNode removeLeafNodes(TreeNode root, int target) {
+		if (root == null)
+			return null;
+
+		root.left = removeLeafNodes(root.left, target);
+		root.right = removeLeafNodes(root.right, target);
+
+		if (root.left == null && root.right == null && target == root.val) 
+			return null;
+		
+		return root;
+	}
+
+
+}
