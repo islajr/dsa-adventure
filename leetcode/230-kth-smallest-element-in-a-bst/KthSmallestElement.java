a5601467a68cb4fa8ee0d21b8b1c7f0a666257ee
Isla <islarcherjr@icloud.com>
2026-04-16 23:00:46 +0100

    leetcode: 230 - kth smallest element in a bst (java)

---
+++ leetcode/230-kth-smallest-element-in-a-bst/KthSmallestElement.java
@@ -0,0 +1,38 @@
+// LC 230: Kth Smallest Element in a BST
+
+class KthSmallestElement {
+	class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {}
+		
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+    public int kthSmallest(TreeNode root, int k) {
+        List<Integer> arr = new ArrayList<>();
+        dfs(root, arr);
+        return arr.get(k - 1);
+
+    }
+
+    private void dfs(TreeNode node, List<Integer> arr) {
+        if (node == null)
+            return;
+        
+        dfs(node.left, arr);
+        arr.add(node.val);
+        dfs(node.right, arr);
+    }
+
+}
