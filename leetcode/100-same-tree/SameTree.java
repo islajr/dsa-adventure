a0a5f9ff9c63bbaa9fec25e8990053b2a486558d
Isla <islarcherjr@icloud.com>
2026-05-05 21:19:44 +0100

    leetcode: 100 - same tree (java)

---
+++ leetcode/100-same-tree/SameTree.java
@@ -0,0 +1,11 @@
+// LC 100: Same Tree
+
+class Solution {
+	public boolean isSameTree(TreeNode p, TreeNode q) {
+		if (p == null && q == null) return true;
+
+		if ((p != null && q != null) && (p.val == q.val)) {
+			return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+		} else return false;
+	}
+}
