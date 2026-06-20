3c36b985a35b455e8c0db36d5e3a23dba9b28f1f
Isla <islarcherjr@icloud.com>
2026-06-20 23:42:33 +0100

    leetcode: 140 - word break ii (python)

---
+++ leetcode/140-word-break-ii/wordbreak_ii.py
@@ -0,0 +1,23 @@
+# LC 140: Word Break II
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        wordDict = set(wordDict)
+
+        def backtrack(i):
+            if i == len(s):
+                res.append(" ".join(curr))
+                return
+            
+            for j in range(i, len(s)):
+                w = s[i:j + 1]
+                if w in wordDict:
+                    curr.append(w)
+                    backtrack(j + 1)
+                    curr.pop()
+        
+        curr = []
+        res = []
+        backtrack(0)
+        return res
+        
