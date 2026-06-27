7f9dab6a11988812fa896a0f07fc4f33b9958e43
Isla <islarcherjr@icloud.com>
2026-06-27 23:04:07 +0100

    leetcode: 217 - contains duplicate (python)

---
+++ leetcode/217-contains-duplicate/contains_duplicate.py
@@ -0,0 +1,12 @@
+# LC 217: Contains Duplicate
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums_set = set()
+        for num in nums:
+            if num in nums_set:
+                return True
+            nums_set.add(num)
+        return False
+        
+        
