d28c57276047f11eaf842a28d160ad032d8dcc91
Isla <islarcherjr@icloud.com>
2026-07-10 23:25:54 +0100

    leetcode: 215 - kth largest element in an array (java)

---
+++ leetcode/215-kth-largest-element-in-an-array/KthLargest.java
@@ -0,0 +1,8 @@
+// LC 215 - Kth Largest Element in an Array
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[nums.length - k];
+    }
+}
