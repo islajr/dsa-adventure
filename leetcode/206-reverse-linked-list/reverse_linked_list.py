2e8ee9ec7dcb7838511ef809d5f85f3d66a87549
Isla <islarcherjr@icloud.com>
2026-09-16 19:40:22 +0100

    leetcode: 206 - reverse linked list (python)

---
+++ leetcode/206-reverse-linked-list/reverse_linked_list.py
@@ -0,0 +1,12 @@
+# LC 206: Reverse Linked List
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, curr = None, head
+
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
